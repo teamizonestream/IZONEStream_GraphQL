6a91d7a04c8efbb65445c39c33111ed1aa3b56ff
Kang Chan <kentakang@hycoding.io>
2019-07-18 23:18:12 +0900

    MongoDB 연결 추가

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,3 +1,14 @@
+import mongoose from "mongoose";
+import database from "../../database.json";
+
+mongoose.connect(
+  `mongodb+srv://${database.username}:${database.password}@${database.url}`,
+  { useNewUrlParser: true },
+  err => {
+    console.log(err);
+  }
+);
+
 const resolvers = {
   Query: {}
 };
