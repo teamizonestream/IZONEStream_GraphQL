a0b8eb4789096344207cea64d5fcd6eab80802e9
Kang Chan <kentakang@hycoding.io>
2019-07-28 19:08:13 +0900

    Date 타입 추가

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,8 +2,26 @@ import { getVideos, getVideoById } from "./Video";
 import { getPhotos, getPhotoById } from "./Photo";
 import { getSchedules, getScheduleById } from "./Schedule";
 import { getCategories, getCategoryById } from "./Category";
+import { GraphQLScalarType } from "graphql";
 
 const resolvers = {
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Date type",
+    parseValue(value) {
+      return new Date(value);
+    },
+    serialize(value) {
+      return value.getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return parseInt(ast.value, 10);
+      }
+
+      return null;
+    }
+  }),
   Query: {
     videos: (_, { category }) => getVideos(category),
     video: (_, { _id }) => getVideoById(_id),
